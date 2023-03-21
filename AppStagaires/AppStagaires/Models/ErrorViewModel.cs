6532c74b70c531dd7abf91969087ef4c804d4988
=LachhabKawtar <=kawtar.lachhab.emsi@gmail.com>
2023-03-21 11:58:18 +0100

    FirstCommit

---
+++ AppStagaires/AppStagaires/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppStagaires.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
