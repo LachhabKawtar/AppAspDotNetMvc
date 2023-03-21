d475837e5ee98e102e1ce1135c982c3de43ace03
=LachhabKawtar <=kawtar.lachhab.emsi@gmail.com>
2023-03-21 12:17:31 +0100

    Etudiant Commit

---
+++ AppStagaires/AppStagaires/Models/Etudiant.cs
@@ -0,0 +1,9 @@
+﻿namespace AppStagaires.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string prenom { get; set; }
+    }
+}
